58dd0854bcdcaee9d7dd38ef64c14e9c8b8edaaf
GuiScarparo <guilhermescarparogrande@gmail.com>
2026-03-10 13:27:14 -0300

    cabado

--- JavaScript/DOM/03032026/atividade.js
+++ JavaScript/DOM/03032026/atividade.js
@@ -1,3 +1,4 @@
+// Atividade 1
 let botao = document.querySelector("#botao");
 let botao2 = document.querySelector("#botao2");
 
@@ -10,3 +11,67 @@ botao2.addEventListener("click", function() {
     let titulo = document.querySelector("#titulo");
     titulo.style.color = "red";
 });
+
+// Atividade 2
+
+let mostrar = document.querySelector("#mostrar");
+mostrar.addEventListener("click", function() {
+    oculto.classList.toggle("mensagem");
+});
+
+// Atividade 3
+
+let input = document.querySelector("#input");
+let contador = document.querySelector("#contador");
+
+input.addEventListener("input", ()=>{
+    contador.textContent = input.value.length;
+});
+
+//atividade 4
+
+let clickbutton = document.querySelector("#clickbutton");
+let contadorCliques = document.querySelector("#contadorCliques");
+
+clickbutton.addEventListener("click", ()=>{
+    let contador = parseInt(contadorCliques.textContent);
+    contador++;
+    contadorCliques.textContent = contador;
+}   );
+
+//atividade 5
+let lista = document.querySelector("#lista");
+let menlista = document.querySelector("#menlista");
+let adicionar = document.querySelector("#adicionar");
+adicionar.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.textContent = menlista.value;          
+    document.querySelector("#lista").appendChild(item);
+    //atividade 6 e desafio extra
+    item.addEventListener("click", function(){
+        let promptResult = prompt("Deseja remover este item? (sim/não)");
+        if(promptResult === "sim"){
+            lista.removeChild(item);
+        }
+        else{            alert("Item não removido.");
+        }
+
+});
+
+});
+
+//atividade HARD
+let mandar = document.querySelector("#mandar");
+let nomeInput = document.querySelector("#nome");
+let mostrarNome = document.querySelector("#mostarNome");    
+
+mandar.addEventListener("click", function(){
+    let nome = nomeInput.value;
+    if(nome === ""){
+        document.querySelector("#mostarNome").innerHTML = "<p id='erro'>O campo nome é obrigatório</p>";
+    }
+    else{
+        document.querySelector("#mostarNome").innerHTML = "<p id='sucesso'>Nome enviado com sucesso!</p>";
+    }
+
+}); 
