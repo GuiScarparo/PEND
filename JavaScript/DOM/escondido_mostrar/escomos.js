2d9375125891c41fbf382a2dea0cb55c840e9958
GuiScarparo <guilhermescarparogrande@gmail.com>
2026-03-17 14:25:33 -0300

    exemplo

---
+++ JavaScript/DOM/escondido_mostrar/escomos.js
@@ -0,0 +1,13 @@
+let senha = document.querySelector("#input");
+let botaoSenha = document.querySelector("#botaoSenha");
+
+botaoSenha.addEventListener("click", function(){
+    if(senha.type === "password"){
+        senha.setAttribute("type", "text");
+        botaoSenha.classList.replace("bi-eye-fill", "bi-eye-slash-fill");
+    }else{
+        senha.setAttribute("type", "password");
+        botaoSenha.classList.replace("bi-eye-slash-fill", "bi-eye-fill");
+    }
+
+});
