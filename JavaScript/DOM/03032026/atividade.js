007c9ab28868060d45ffb05a15e5f09c21082884
GuiScarparo <guilhermescarparogrande@gmail.com>
2026-03-24 14:25:45 -0300

    atividade

--- JavaScript/DOM/03032026/atividade.js
+++ JavaScript/DOM/03032026/atividade.js
@@ -2,21 +2,21 @@
 let botao = document.querySelector("#botao");
 let botao2 = document.querySelector("#botao2");
 
-botao.addEventListener("click", function() {
-    let titulo = document.querySelector("#titulo");
-    titulo.style.color = "blue";
+botao.addEventListener("click", function () {
+  let titulo = document.querySelector("#titulo");
+  titulo.style.color = "blue";
 });
 
-botao2.addEventListener("click", function() {
-    let titulo = document.querySelector("#titulo");
-    titulo.style.color = "red";
+botao2.addEventListener("click", function () {
+  let titulo = document.querySelector("#titulo");
+  titulo.style.color = "red";
 });
 
 // Atividade 2
 
 let mostrar = document.querySelector("#mostrar");
-mostrar.addEventListener("click", function() {
-    oculto.classList.toggle("mensagem");
+mostrar.addEventListener("click", function () {
+  oculto.classList.toggle("mensagem");
 });
 
 // Atividade 3
@@ -24,8 +24,8 @@ mostrar.addEventListener("click", function() {
 let input = document.querySelector("#input");
 let contador = document.querySelector("#contador");
 
-input.addEventListener("input", ()=>{
-    contador.textContent = input.value.length;
+input.addEventListener("input", () => {
+  contador.textContent = input.value.length;
 });
 
 //atividade 4
@@ -33,82 +33,98 @@ input.addEventListener("input", ()=>{
 let clickbutton = document.querySelector("#clickbutton");
 let contadorCliques = document.querySelector("#contadorCliques");
 
-clickbutton.addEventListener("click", ()=>{
-    let contador = parseInt(contadorCliques.textContent);
-    contador++;
-    contadorCliques.textContent = contador;
-}   );
+clickbutton.addEventListener("click", () => {
+  let contador = parseInt(contadorCliques.textContent);
+  contador++;
+  contadorCliques.textContent = contador;
+});
 
 //atividade 5
 let lista = document.querySelector("#lista");
 let menlista = document.querySelector("#menlista");
 let adicionar = document.querySelector("#adicionar");
-adicionar.addEventListener("click", function(){
-    let item = document.createElement("li");
-    item.textContent = menlista.value;          
-    document.querySelector("#lista").appendChild(item);
-    //atividade 6 e desafio extra
-    item.addEventListener("click", function(){
-        let promptResult = prompt("Deseja remover este item? (sim/não)");
-        if(promptResult === "sim"){
-            lista.removeChild(item);
-        }
-        else{            alert("Item não removido.");
-        }
-
-});
-
+adicionar.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.textContent = menlista.value;
+  document.querySelector("#lista").appendChild(item);
+  //atividade 6 e desafio extra
+  item.addEventListener("click", function () {
+    let promptResult = prompt("Deseja remover este item? (sim/não)");
+    if (promptResult === "sim") {
+      lista.removeChild(item);
+    } else {
+      alert("Item não removido.");
+    }
+  });
 });
 
 //atividade HARD
 let mandar = document.querySelector("#mandar");
 let nomeInput = document.querySelector("#nome");
-let mostrarNome = document.querySelector("#mostarNome");    
+let mostrarNome = document.querySelector("#mostarNome");
 
-mandar.addEventListener("click", function(){
-    let nome = nomeInput.value;
-    if(nome === ""){
-        document.querySelector("#mostarNome").innerHTML = "<p class='erro'>O campo nome é obrigatório</p>";
-    }
-    else{
-        document.querySelector("#mostarNome").innerHTML = "<p class='sucesso'>Nome enviado com sucesso!</p>";
-    }
-
-}); 
+mandar.addEventListener("click", function () {
+  let nome = nomeInput.value;
+  if (nome === "") {
+    document.querySelector("#mostarNome").innerHTML =
+      "<p class='erro'>O campo nome é obrigatório</p>";
+  } else if (nome === " ") {
+    document.querySelector("#mostarNome").innerHTML =
+      "<p class='erro'>O campo nome é obrigatório</p>";
+  } else if (nome === "\S" + " " + "\S") {
+    document.querySelector("#mostarNome").innerHTML =
+      "<p class='sucesso'>Nome enviado com sucesso!</p>";
+  } else {
+    document.querySelector("#mostarNome").innerHTML =
+      "<p class='sucesso'>Nome enviado com sucesso!</p>";
+  }
+});
 
 //exercicio validação de campos
 
 let enviarEmail = document.querySelector("#enviarEmail");
 let emailInput = document.querySelector("#email");
 let mostrarEmail = document.querySelector("#mostrarEmail");
-
-
-enviarEmail.addEventListener("click", function(){
-    let email = emailInput.value; 
-    let arroba = email.indexOf("@");
-    let ponto = email.lastIndexOf(".");
-    if(arroba > 0 && ponto > arroba + 1 && ponto < email.length - 1){
-        document.querySelector("#mostrarEmail").innerHTML = "<p class='sucesso'>Email enviado com sucesso!</p>";
-        return true;
-    }
-    else{
-        document.querySelector("#mostrarEmail").innerHTML = "<p class='erro'>erro.</p>";
-        return false;
-    }
-});
-
-
-//verifica a força da senha
-
 let senhaInput = document.querySelector("#senha");
 let mostrarSenha = document.querySelector("#mostrarSenha");
 
-senhaInput.addEventListener("input", function(){
-    let contaSenha = senhaInput.value.length;
-    if(contaSenha < 6){
-        mostrarSenha.innerHTML = "<p class='erro'>Senha fraca</p>";
+senhaInput.addEventListener("input", function () {
+  let contaSenha = senhaInput.value.length;
+  if (contaSenha < 6) {
+    mostrarSenha.innerHTML = "<p class='erro'>Senha fraca</p>";
+  } else if (contaSenha >= 6 && contaSenha < 10) {
+    mostrarSenha.innerHTML = "<p class='passou'>Senha aceitavel</p>";
+  } else {
+    mostrarSenha.innerHTML = "<p class='sucesso'>Senha forte</p>";
+  }
+});
+
+enviarEmail.addEventListener("click", function () {
+  contaSenha = senhaInput.value.length;
+  let email = emailInput.value;
+  let arroba = email.indexOf("@");
+  let ponto = email.lastIndexOf(".");
+  let emailValido =
+    arroba > 0 && ponto > arroba + 1 && ponto < email.length - 1;
+  let senhaValida = contaSenha >= 6;
+  if (emailValido && senhaValida) {
+    document.querySelector("#mostrarEmail").innerHTML =
+      "<p class='sucesso'>Email e senha enviados   com sucesso!</p>";
+    return true;
+  } else {
+    if (emailValido === false && senhaValida === false) {
+      document.querySelector("#mostrarEmail").innerHTML =
+        "<p class='erro'>Email e senha inválidos, verifique os campos e tente novamente.</p>";
+      return false;
     }
-    else{
-        mostrarSenha.innerHTML = "<p class='sucesso'>Senha forte</p>";
+    if (senhaValida === false) {
+      document.querySelector("#mostrarEmail").innerHTML =
+        "<p class='erro'>senha fraca, digite uma senha com pelo menos 6 caracteres.</p>";
+      return false;
+    } else {
+      document.querySelector("#mostrarEmail").innerHTML =
+        "<p class='erro'>Email inválido, email sem @ ou ponto pós arroba</p>";
+      return false;
     }
+  }
 });
