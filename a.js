c0d162d6fbfbc480c522cd030ffb55c89fa7d5fc
GuiScarparo <guilhermescarparogrande@gmail.com>
2026-05-19 11:25:29 -0300

    media query

---
+++ a.js
@@ -0,0 +1,16 @@
+function nomeDaFuncao(parametro1, parametro2) {
+  // instruções
+  return resultado;
+}
+const minhaFuncao = function(parametro1) {
+  // instruções
+  return resultado;
+};
+const minhaFuncao = (parametro1) => {
+  // instruções
+  return resultado;
+};
+
+// Para funções com apenas uma expressão e um único parâmetro,
+//  pode ser ainda mais concisa:
+const dobrar = numero => numero * 2;
